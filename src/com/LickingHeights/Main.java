6fdcd3ac13f765b9e1a4cb69dea553bafeb48036
Erin Stoker <erinstokermusic@gmail.com>
2020-01-31 12:07:24 -0500

    f_i input added

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+drakeEquation();
 
     }
 
@@ -34,19 +34,24 @@ Scanner keyboard;
 keyboard = new Scanner(System.in);
 
 System.out.println("Give me the rate at which stars form in the Milky Way.");
-rStar = keyboard.nextLine();
+rStar = keyboard.nextInt();
 System.out.println("Give me the fraction of those stars that have planets orbiting them.");
-f_p = keyboard.nextLine();
+f_p = keyboard.nextDouble();
 System.out.println("Give me the fraction of those planets that reside in the goldilocks zone.");
-numberOfExoplanets = keyboard.nextLine();
+numberOfExoplanets = keyboard.nextInt();
 System.out.println("Give me the fraction of those planets that have life.");
-f_l = keyboard.nextLine();
+f_l = keyboard.nextDouble();
+System.out.println("Give me the fraction of those planets with life that have " +
+        "the capacity to develop intelligent life.");
+f_i = keyboard.nextDouble();
+// System.out.println("Give me the fraction of that intelligent life that" +
+      //  " will be able to communicate in a way we can understand");
 
-N = rStar * f_p * f_l * f_i * f_c * L;
+N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
 
 
 System.out.println("The number of intelligent" +
-        "species that are out there\nand can communicate with us is" + N)''
+        "species that are out there\nand can communicate with us is" + N);
     }
 
 }
